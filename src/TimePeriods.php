5c29308768d54b7ef252a7c570c7e28bda2e090c
Stephen Neal <stephen.neal14@gmail.com>
2020-10-13 11:20:54 -0400

    MAKE TimePeriods class for retrieving time period strings
    ADD sfneal/array-helpers to compoer requirements.

---
+++ src/TimePeriods.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Sfneal\Helpers\Time;
+
+use Sfneal\Actions\AbstractService;
+
+class TimePeriods extends AbstractService
+{
+    /**
+     * Names of helper methods that don't return time periods.
+     */
+    const HELPER_METHODS = ['all', 'get', 'getPeriod', 'mapMethods'];
+
+    /**
+     * Retrieve an array time period names and time period ranges.
+     *
+     * @param array $exclusions
+     * @return array
+     */
+    public static function all(array $exclusions = [])
+    {
+        // Get all of the methods in this class except 'all' & 'getPeriod'
+        $methods = array_filter(get_class_methods(new self()), function ($method) use ($exclusions) {
+            return ! in_array($method, array_unique(array_merge(self::HELPER_METHODS, $exclusions)));
+        });
+
+        // Method name key, with method results values
+        return self::mapMethods($methods);
+    }
+
+    /**
+     * Retrieve an array time period names and time period ranges.
+     *
+     * @param array $inclusions
+     * @return array
+     */
+    public static function get(...$inclusions)
+    {
+        // Get all of the methods in this class except 'all' & 'getPeriod'
+        $methods = array_filter(get_class_methods(new self()), function ($method) use ($inclusions) {
+            return in_array($method, $inclusions);
+        });
+
+        // Method name key, with method results values
+        return self::mapMethods($methods);
+    }
+
+    /**
+     * Retrieve a single period range.
+     *
+     * @param $period
+     * @return mixed
+     */
+    public static function timePeriod($period)
+    {
+        return self::{$period}();
+    }
+
+    /**
+     * This Month.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function thisMonth(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('first day of this month')).' 00:00:00',
+            date($format, strtotime('last day of this month')).' 23:59:59',
+        );
+    }
+
+    /**
+     * Last Month.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function lastMonth(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('first day of last month')).' 00:00:00',
+            date($format, strtotime('last day of last month')).' 23:59:59',
+        );
+    }
+
+    /**
+     * This Week.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function thisWeek(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('Monday this week')).' 00:00:00',
+            date($format, strtotime('Sunday this week')).' 23:59:59',
+        );
+    }
+
+    /**
+     * Today.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function today(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('Today')).' 00:00:00',
+            date($format, strtotime('Today')).' 23:59:59',
+        );
+    }
+
+    /**
+     * Yesterday.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function yesterday(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('Yesterday')).' 00:00:00',
+            date($format, strtotime('Yesterday')).' 23:59:59',
+        );
+    }
+
+    /**
+     * Tomorrow.
+     *
+     * @param $format string Date format
+     * @return array
+     */
+    public static function tomorrow(string $format = 'Y-m-d')
+    {
+        return self::getPeriod(
+            date($format, strtotime('Tomorrow')).' 00:00:00',
+            date($format, strtotime('Tomorrow')).' 23:59:59',
+        );
+    }
+
+    /**
+     * Return an array of time periods with 'start' and 'end' keys.
+     *
+     * @param string $start
+     * @param string $end
+     * @return array
+     */
+    private static function getPeriod(string $start, string $end): array
+    {
+        return [$start, $end];
+    }
+
+    /**
+     * Return method name key, with method results values.
+     *
+     * @param array $methods
+     * @return array
+     */
+    private static function mapMethods(array $methods)
+    {
+        return arrayFlattenKeys(array_map(function ($method, $times) {
+            return [$method => $times];
+        },
+            $methods,
+            array_map(function ($method) {
+                return self::{$method}();
+            }, $methods)
+        ), false);
+    }
+}
