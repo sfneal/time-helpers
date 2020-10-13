d4299cc5a6223af14354f9b0fd7253f1b27e0b67
Stephen Neal <stephen.neal14@gmail.com>
2020-10-13 14:29:13 -0400

    ADD to TimePeriodsTest

--- tests/TimePeriodsTest.php
+++ tests/TimePeriodsTest.php
@@ -10,28 +10,72 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function timeThisMonth()
     {
-        $period = TimePeriods::thisMonth();
-        $this->assertTrue(is_array($period));
+        list($start, $end) = TimePeriods::thisMonth();
+
+        $targetStart = date('Y-m-d', strtotime('first day of this month')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('last day of this month')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
     }
 
     /** @test */
     public function timeLastMonth()
     {
-        $period = TimePeriods::lastMonth();
-        $this->assertTrue(is_array($period));
+        list($start, $end) = TimePeriods::lastMonth();
+
+        $targetStart = date('Y-m-d', strtotime('first day of last month')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('last day of last month')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
     }
 
     /** @test */
     public function timeThisWeek()
     {
-        $period = TimePeriods::thisWeek();
-        $this->assertTrue(is_array($period));
+        list($start, $end) = TimePeriods::thisWeek();
+
+        $targetStart = date('Y-m-d', strtotime('Monday this week')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('Sunday this week')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
     }
 
     /** @test */
-    public function timeToday()
+    public function today()
     {
-        $period = TimePeriods::today();
-        $this->assertTrue(is_array($period));
+        list($start, $end) = TimePeriods::today();
+
+        $targetStart = date('Y-m-d', strtotime('Today')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('Today')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
+    }
+
+    /** @test */
+    public function yesterday()
+    {
+        list($start, $end) = TimePeriods::yesterday();
+
+        $targetStart = date('Y-m-d', strtotime('Yesterday')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('Yesterday')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
+    }
+
+    /** @test */
+    public function tomorrow()
+    {
+        list($start, $end) = TimePeriods::tomorrow();
+
+        $targetStart = date('Y-m-d', strtotime('Tomorrow')).' 00:00:00';
+        $targetEnd = date('Y-m-d', strtotime('Tomorrow')).' 23:59:59';
+
+        $this->assertTrue($start == $targetStart);
+        $this->assertTrue($end == $targetEnd);
     }
 }
