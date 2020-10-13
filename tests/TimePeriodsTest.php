f38c87025534b2cf7c32110c1efc791932420aa8
Stephen Neal <stephen.neal14@gmail.com>
2020-10-13 14:30:14 -0400

    Apply fixes from StyleCI (#2)

--- tests/TimePeriodsTest.php
+++ tests/TimePeriodsTest.php
@@ -10,7 +10,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function thisMonth()
     {
-        list($start, $end) = TimePeriods::thisMonth();
+        [$start, $end] = TimePeriods::thisMonth();
 
         $targetStart = date('Y-m-d', strtotime('first day of this month')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('last day of this month')).' 23:59:59';
@@ -22,7 +22,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function lastMonth()
     {
-        list($start, $end) = TimePeriods::lastMonth();
+        [$start, $end] = TimePeriods::lastMonth();
 
         $targetStart = date('Y-m-d', strtotime('first day of last month')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('last day of last month')).' 23:59:59';
@@ -34,7 +34,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function thisWeek()
     {
-        list($start, $end) = TimePeriods::thisWeek();
+        [$start, $end] = TimePeriods::thisWeek();
 
         $targetStart = date('Y-m-d', strtotime('Monday this week')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('Sunday this week')).' 23:59:59';
@@ -46,7 +46,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function today()
     {
-        list($start, $end) = TimePeriods::today();
+        [$start, $end] = TimePeriods::today();
 
         $targetStart = date('Y-m-d', strtotime('Today')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('Today')).' 23:59:59';
@@ -58,7 +58,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function yesterday()
     {
-        list($start, $end) = TimePeriods::yesterday();
+        [$start, $end] = TimePeriods::yesterday();
 
         $targetStart = date('Y-m-d', strtotime('Yesterday')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('Yesterday')).' 23:59:59';
@@ -70,7 +70,7 @@ class TimePeriodsTest extends TestCase
     /** @test */
     public function tomorrow()
     {
-        list($start, $end) = TimePeriods::tomorrow();
+        [$start, $end] = TimePeriods::tomorrow();
 
         $targetStart = date('Y-m-d', strtotime('Tomorrow')).' 00:00:00';
         $targetEnd = date('Y-m-d', strtotime('Tomorrow')).' 23:59:59';
