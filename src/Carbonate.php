7f071db75ce920198cc34f2a2edcf73c8ddb2ea3
Stephen Neal <stephen.neal14@gmail.com>
2021-02-16 10:36:37 -0500

    MAKE Carbonate service for adding or subtracting days to the current date
    MAKE CarbonateTest for testing new class & methods.

---
+++ src/Carbonate.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Sfneal\Helpers\Time;
+
+
+use Carbon\Carbon;
+use Sfneal\Actions\AbstractService;
+
+class Carbonate extends AbstractService
+{
+    // todo: add year, month & week methods
+    // todo: add methods that accept positives or negative ints to determine sub or add
+
+    /**
+     * Create a Carbon datetime object representing $x days ago.
+     *
+     * @param int $daysAgo
+     * @return Carbon
+     */
+    public static function daysAgo(int $daysAgo): Carbon
+    {
+        return Carbon::now()->subDays($daysAgo);
+    }
+
+    /**
+     * Create a Carbon datetime object representing $x days from now.
+     *
+     * @param int $daysFromNow
+     * @return Carbon
+     */
+    public static function daysHence(int $daysFromNow): Carbon
+    {
+        return Carbon::now()->addDays($daysFromNow);
+    }
+}
---
+++ tests/CarbonateTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Sfneal\Helpers\Time\Tests;
+
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+use Sfneal\Helpers\Time\Carbonate;
+use Sfneal\Helpers\Time\Period;
+
+class CarbonateTest extends TestCase
+{
+    /**
+     * @var int
+     */
+    private $year;
+
+    /**
+     * @var int
+     */
+    private $month;
+
+    /**
+     * @var int
+     */
+    private $day;
+
+    /**
+     * This method is called before each test.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Set random year, month & day
+        $this->year = rand(1980, Carbon::today()->year);
+        $this->month = rand(1, 12);
+        $this->day = rand(1, 28);
+
+        // Change current date for testing purposes
+        Carbon::setTestNow(Carbon::create($this->year, $this->month, $this->day));
+
+    }
+
+    /** @test */
+    public function it_can_subtract_days()
+    {
+        $daysAgo = rand(0, $this->day);
+        $expected = Carbon::create($this->year, $this->month, $this->day - $daysAgo);
+        $output = Carbonate::daysAgo($daysAgo);
+
+        $this->assertInstanceOf(Carbon::class, $output);
+        $this->assertEquals($expected, $output);
+    }
+
+    /** @test */
+    public function it_can_add_days()
+    {
+        $daysAgo = rand(0, 30);
+        $expected = Carbon::create($this->year, $this->month, $this->day + $daysAgo);
+        $output = Carbonate::daysHence($daysAgo);
+
+        $this->assertInstanceOf(Carbon::class, $output);
+        $this->assertEquals($expected, $output);
+    }
+}
