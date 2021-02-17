e03f2abbbf4d1a880284914041d7fe6f4a8ef8e4
Stephen Neal <stephen.neal14@gmail.com>
2021-02-17 11:43:58 -0500

    MAKE DurationTraitTest for testing use of Duration trait
    ADD properties to phpdocs & reformat todos.

--- src/Traits/Duration.php
+++ src/Traits/Duration.php
@@ -8,7 +8,10 @@ use Sfneal\Helpers\Time\TimeConverter;
  * A time value attributes to an Eloquent Model.
  *
  * @property $total_duration
+ * @property $total_hours
+ * @property $total_minutes
  * @property $total_seconds
+ * @property $total_time
  */
 trait Duration
 {
@@ -16,6 +19,8 @@ trait Duration
 
     /**
      * Retrieve the total duration in minutes with decimals.
+     *
+     * // todo: refactor to protected method for overriding & default functionality
      *
      * @return float
      */
@@ -34,6 +39,8 @@ trait Duration
     /**
      * Retrieve total duration converted to minutes without decimals.
      *
+     * // todo: fix return type to integer
+     *
      * @return false|float
      */
     public function getTotalMinutesAttribute()
@@ -43,6 +50,8 @@ trait Duration
 
     /**
      * Retrieve total duration converted to seconds.
+     *
+     * todo: fix return type to integer
      *
      * @return float|int
      */
